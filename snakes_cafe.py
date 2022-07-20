119b343afb8baa877f6343f066060c8f81712174
Brentice Loper <brentice@icloud.com>
2022-07-20 00:41:04 -0700

    Code fixed and working.

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -30,6 +30,40 @@ Unicorn Tears
 ** What would you like to order? **
 ***********************************
 """)
+#intialize empty meal dictionary
+items = {
+    "Wings": 0,
+    "Cookies": 0,
+    "Spring Rolls": 0,
+    "Salmon": 0,
+    "Steak": 0,
+    "Meat Tornado": 0,
+    "A Literal Garden": 0,
+    "Ice Cream": 0,
+    "Cake": 0,
+    "Pie": 0,
+    "Coffee": 0,
+    "Tea": 0,
+    "Unicorn Tears": 0
+}
 
-while order != 'Quit':
-    order = input('> ')
+# print the order
+num_items = 1 #TODO: properly tally items that have been ordered
+
+order = input("> ")
+
+while order != "quit":
+    if order not in items:
+        print("Please order from the menu.")
+        order = input("> ")
+        continue
+    if items[order] == 0:
+        items[order] = 1
+        report = f"** 1 order of {order} has been added to your meal **"
+        print(report)
+    else:
+        items[order] += 1
+        report = f"** {items[order]} orders of {order} have been added to your meal **"
+        print(report)
+
+    order = input("> ")
